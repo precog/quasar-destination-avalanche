ece9156d2271f6e6f665648c775ff65ff386ebbe
Becky Conning <becky.conning@icloud.com>
2020-05-04 20:10:02 +0100

    Added writemode to avalanche destination

---
+++ core/src/main/scala/quasar/destination/avalanche/WriteMode.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Precog Data
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.destination.avalanche
+
+import slamdata.Predef.{Eq => _, _}
+
+import argonaut._, Argonaut._
+
+import cats._
+
+sealed trait WriteMode extends Product with Serializable
+
+object WriteMode {
+  case object Create extends WriteMode
+  case object Replace extends WriteMode
+  case object Truncate extends WriteMode
+
+  implicit val codecJson: CodecJson[WriteMode] =
+    CodecJson(
+      _ match {
+        case Create => jString("create")
+        case Replace => jString("replace")
+        case Truncate => jString("truncate")
+      },
+      c => c.as[String] flatMap {
+        case "create" => DecodeResult.ok(Create)
+        case "replace" => DecodeResult.ok(Replace)
+        case "truncate" => DecodeResult.ok(Truncate)
+        case _ => DecodeResult.fail("Valid write modes are 'create', 'truncate' and 'replace'", c.history)
+      })
+
+  implicit val eqv: Eq[WriteMode] =
+    Eq.fromUniversalEquals
+}
